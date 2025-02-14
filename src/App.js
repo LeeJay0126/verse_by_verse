c19e505b7994d7025838be11b3b68bce31e6864b
LeeJay0126 <topaz9889@gmail.com>
2025-02-13 16:36:08 -0800

    Logo and Header created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './component/Header';
 import Home from "./home/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header/>
       </header>
       <Home/>
     </div>
---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+import Logo from "./Logo";
+
+const Header = () => {
+
+    return (
+        <header className="Header">
+            <Logo />
+        </header>
+    );
+};
+
+export default Header;
