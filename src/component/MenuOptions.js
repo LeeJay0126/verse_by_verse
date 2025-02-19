2a42c362afcbd413c1dd49fc735ac9d9df60f193
LeeJay0126 <topaz9889@gmail.com>
2025-02-19 15:07:52 -0800

    Menu Options fix

--- src/component/MenuOptions.js
+++ src/component/MenuOptions.js
@@ -6,7 +6,7 @@ const MenuOptions = () => {
         <ul className='MenuOptions'>
             <li className='MenuOptionList'>HOME</li>
             <li className='MenuOptionList'>ABOUT</li>
-            <li className='MenuOptionList'>SELF STUDY</li>
+            <li className='MenuOptionList'>STUDY & REFLECT</li>
             <li className='MenuOptionList'>COMMUNITIES</li>
             <li className='MenuOptionList'>CONTACT</li>
             <li className='MenuOptionList'>ACCOUNT</li>
