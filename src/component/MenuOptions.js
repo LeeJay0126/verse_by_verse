a9449108043c9f212c74c7150cf7005ed5f127ca
LeeJay0126 <topaz9889@gmail.com>
2025-03-12 14:06:10 -0700

    Link applied to MenuOptions

--- src/component/MenuOptions.js
+++ src/component/MenuOptions.js
@@ -1,10 +1,12 @@
+import Home from '../home/Home';
 import './MenuOptions.css';
+import { Link } from 'react-router-dom';
 
 const MenuOptions = () => {
 
     return (
         <ul className='MenuOptions'>
-            <li className='MenuOptionList'>HOME</li>
+            <li className='MenuOptionList'><Link to='/' element={<Home/>}>HOME</Link></li>
             <li className='MenuOptionList'>ABOUT</li>
             <li className='MenuOptionList'>STUDY & REFLECT</li>
             <li className='MenuOptionList'>COMMUNITIES</li>
