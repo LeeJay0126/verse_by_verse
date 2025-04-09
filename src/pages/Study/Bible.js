1d1a3518fbfbf2e1c29cab2cd5344b2a4e62edd6
Jay Seung Yeon Lee <topaz9889@gmail.com>
2025-04-08 19:47:40 -0700

    Study / Read Page under construction

---
+++ src/pages/Study/Bible.js
@@ -0,0 +1,13 @@
+import './Bible.css';
+import BookVersion from './BookVersion';
+
+const Bible = () => {
+
+    return (
+        <section className="ReadBible">
+            <BookVersion/>
+        </section>
+    );
+};
+
+export default Bible;
--- src/pages/Study/Study.js
+++ src/pages/Study/Study.js
@@ -1,16 +1,21 @@
-import BibleHeader from '../Read/BibleHeader';
-import './Study.css';
-import image from "../../component/images/homePage/mobileSection/TableWithOpenBible.jpg";
+import BibleHeader from "../Read/BibleHeader";
+import Bible from "./Bible";
+import "./Study.css";
 
 const Study = () => {
-
-    return (
-        <section className='Study'>
-            <div className='StudyHero'>
-                <BibleHeader />
-            </div>
-        </section>
-    );
+  return (
+    <section className="Study">
+      <div className="StudyHero">
+        <BibleHeader />
+        <h1 className="StudyHeader">Read, Record, and Reflect</h1>
+        <h2 className="StudyH2">
+          Read the bible verse by verse and record your insights on Verse By
+          Verse
+        </h2>
+      </div>
+      <Bible />
+    </section>
+  );
 };
 
 export default Study;
