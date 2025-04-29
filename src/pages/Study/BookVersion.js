1a2290751298044af92d8c95b52327ffb4957161
LeeJay0126 <topaz9889@gmail.com>
2025-04-28 20:16:00 -0700

    Componentized GetBibleVersions Function

--- src/pages/Study/BookVersion.js
+++ src/pages/Study/BookVersion.js
@@ -3,34 +3,6 @@ import { GoTriangleDown } from "react-icons/go";
 
 const BookVersion = () => {
 
-    const API_KEY = "ea9272ba0be86f8024c97d566497f993";
-
-    const getBibleVersions = () => {
-        return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-            xhr.withCredentials = false;
-            xhr.addEventListener(`readystatechange`, function () {
-                if (this.readyState === this.DONE) {
-                    const { data } = JSON.parse(this.responseText);
-                    const versions = data.map((data) => {
-                        return {
-                            name: data.name,
-                            id: data.id,
-                            abbreviation: data.abbreviation,
-                            description: data.description,
-                            language: data.language.name,
-                        };
-                    });
-                    resolve(versions);
-                }
-            });
-            xhr.open(`GET`, `https://api.scripture.api.bible/v1/bibles`);
-            xhr.setRequestHeader(`api-key`, API_KEY);
-            xhr.onerror = () => reject(xhr.statusText);
-            xhr.send();
-        });
-    };
-
     return (
         <div className="BookVersionHolder">
             <div className="Books">
