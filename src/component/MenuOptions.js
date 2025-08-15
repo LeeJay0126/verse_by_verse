052f11d151f916321f2d836eb01e2b9339288955
LeeJay0126 <topaz9889@gmail.com>
2025-08-14 21:55:59 -0700

    about page deleted, multiple styling edits
    -about page deleted. About section will be added under the home cards section
    -sidebar now appears from the right side
    -submenu for study & reflect is removed

--- src/component/MenuOptions.js
+++ src/component/MenuOptions.js
@@ -7,24 +7,11 @@ const MenuOptions = ({ page = true, onNavigate }) => {
   return (
     <ul className="MenuOptions">
       <li className={item}><Link to="/" onClick={onNavigate}>HOME</Link></li>
-      <li className={item}><Link to="/about" onClick={onNavigate}>ABOUT</Link></li>
-
-      {/* Dropdown using <details> (native, a11y-friendly) */}
-      <li className={`${item} has-submenu`}>
-        <details>
-          <summary>STUDY &amp; REFLECT</summary>
-          <ul className="Submenu">
-            <li><Link to="/study" onClick={onNavigate}>All Studies</Link></li>
-            <li><Link to="/study/old-testament" onClick={onNavigate}>Old Testament</Link></li>
-            <li><Link to="/study/new-testament" onClick={onNavigate}>New Testament</Link></li>
-          </ul>
-        </details>
-      </li>
-
+      <li className={`${item}`}><Link to="/study" onClick={onNavigate}>STUDY &amp; REFLECT</Link>  </li>
       <li className={item}><Link to="/community" onClick={onNavigate}>COMMUNITIES</Link></li>
       <li className={item}><Link to="/contact" onClick={onNavigate}>CONTACT</Link></li>
       <li className={item}><Link to="/account" onClick={onNavigate}>ACCOUNT</Link></li>
-    </ul>
+    </ul >
   );
 };
 
